3a2400928856f518bb8953b432b2450ba21ca78f
Chris <c.bailey00@hotmail.com>
2022-11-11 10:28:45 +0000

    finished test_single_class method

--- single_class/lib/grammarstats.rb
+++ single_class/lib/grammarstats.rb
@@ -1,10 +1,22 @@
 class GrammarStats
 
+  def initialize
+    @success = 0
+    @fail = 0
+  end
+
   def check(text)
-    if text[0] == text[0].upcase
+    punctuation = [".","!","?"]
+    if text[0] == text[0].upcase && punctuation.include?(text[-1])
+      @success += 1
       return true
     else
+      @fail += 1
       return false
     end
+  end
+
+  def percentage_good
+    return @success * 100 / (@success + @fail)
   end
 end
--- single_class/spec/grammarstats_spec.rb
+++ single_class/spec/grammarstats_spec.rb
@@ -4,26 +4,69 @@ RSpec.describe GrammarStats do
   
   describe "#check" do
     context "returns true" do
-      it "Hello." do
+      it "Ends with ." do
         text = GrammarStats.new
         expect(text.check("Hello.")).to eq true
       end
 
-      it "My name is Chris!" do
+      it "Ends with !" do
         text = GrammarStats.new
-        expect(text.check("My name is Chris.")).to eq true
+        expect(text.check("My name is Chris!")).to eq true
+      end
+
+      it "Ends with ?" do
+        text = GrammarStats.new
+        expect(text.check("What's yours?")).to eq true
       end
     end
 
     context "returns false" do
-      it "hello." do
+      it "No capital letter" do
         text = GrammarStats.new
         expect(text.check("hello.")).to eq false
       end
 
-      it "Hello" do
+      it "No full stop" do
+        text = GrammarStats.new
+        expect(text.check("Hello")).to eq false
+      end
+    end
+  end 
+
+  describe "#percentage_good" do
+    it "returns 100% after 1 successful check" do
+      text = GrammarStats.new
+      text.check("Hello.")
+      expect(text.percentage_good).to eq 100
+    end
+
+    it "returns 0% after 1 failed check" do
+      text = GrammarStats.new
+      text.check("hello")
+      expect(text.percentage_good).to eq 0
+    end 
+
+    it "returns 50% after 1 successful and 1 failed check" do
+      text = GrammarStats.new
+      text.check("Hello.")
+      text.check("hello")
+      expect(text.percentage_good).to eq 50
+    end
+
+    it "returns 33%" do
+      text = GrammarStats.new
+      text.check("hello")
+      text.check("hello.")
+      text.check("Hello.")
+      expect(text.percentage_good).to eq 33
+    end
+
+    it "returns 66%" do
+      text = GrammarStats.new
+      text.check("hello")
+      text.check("Chris!")
+      text.check("Hello.")
+      expect(text.percentage_good).to eq 66
     end
   end
-
-      
 end
